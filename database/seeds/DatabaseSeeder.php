330c1deb5429f233edd2a6d2eb18aa4a566443f9
Nathan Godard <nathan.godard1@ucalgary.ca>
2019-04-11 12:25:51 -0600

    yup

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+      {
+        $data = file_get_contents("https://randomuser.me/api/?results=100&nat=us&inc=name,email,login&seed=seng401");
+          $data = json_decode($data, true);
+
+        for($x = 0; $x < 100; $x++){
+      //   $data = json_decode($data);
+
+          DB::table('users')->insert([
+            //'name' => $data['name']['first']." ".$data['name']['last'],
+            //'name' => $data['results']['name']['first']." ".$data['results']['name']['last'],
+            'name' => $data['results'][$x]['name']['first']." ".$data['results'][$x]['name']['last'],
+            'email' => $data['results'][$x]['email'],
+            'password' => $data['results'][$x]['login']['password'],
+          ]);
+
+
+
+
+        }
+
+          //https://randomuser.me/api/?inc=name,email,login
+      }
     }
 }
